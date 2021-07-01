9b99a344bf0e1c61e5900ad7365590e37e1687bf
Kubilay Humm <lieqb2@gmail.com>
2021-07-01 11:13:11 +0300

    tw init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='text-5xl flex items-center justify-center h-screen w-screen bg-blue-300'>
+        merhaba app.tsx
+      </div>
     </div>
   );
-}
-
-export default App;
+};
