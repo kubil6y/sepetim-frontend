4b167172021d98c06cda7a481e6c9e8fff33abd8
Kubilay Humm <lieqb2@gmail.com>
2021-07-05 16:33:21 +0300

    dish card funcs added

--- src/components/DishCard.tsx
+++ src/components/DishCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import {
   getRestaurantQuery_getRestaurant_restaurant_menu,
   getRestaurantQuery_getRestaurant_restaurant_menu_options,
@@ -6,6 +6,14 @@ import {
 import { OrderButton } from './OrderButton';
 
 const nums = [1, 2, 3, 4, 5];
+
+const initialOption: getRestaurantQuery_getRestaurant_restaurant_menu_options = {
+  __typename: 'DishOption',
+  id: 0,
+  extra: 0,
+  name: '',
+  calorie: 0,
+};
 
 interface IDishProps {
   dish: getRestaurantQuery_getRestaurant_restaurant_menu;
@@ -15,12 +23,31 @@ export const DishCard: FC<IDishProps> = ({
   dish: { name, image, calorie, basePrice, options },
 }) => {
   // dishOptionId state
-  const [optionId, setOptionId] = useState<string | undefined>(undefined);
-  // quantity state
+  const [optionId, setOptionId] = useState<number | undefined>(undefined);
+  const [option, setOption] = useState(initialOption);
+
+  useEffect(() => {
+    const main = () => {
+      if (options) {
+        if (optionId === 0) {
+          return setOption(initialOption);
+        }
+        const target = options.find((option) => option.id === optionId);
+        if (target) {
+          setOption(target);
+        }
+      }
+    };
+    main();
+  }, [optionId]);
+
+  // quantity state;
   const [qty, setQty] = useState(1);
 
-  console.log('optionId', optionId);
-  console.log(options);
+  console.log(option);
+
+  const totalPrice = (basePrice + option.extra) * qty;
+  const totalCalories = (calorie + option.calorie) * qty;
 
   return (
     <div className='flex items-center justify-between p-4 border border-gray-200 hover:border-gray-500 cst-transition'>
@@ -34,9 +61,12 @@ export const DishCard: FC<IDishProps> = ({
               <select
                 className='w-3/5 text-xs tracking-wide lowercase'
                 value={optionId}
-                onChange={(e) => setOptionId(e.target.value)}
+                onChange={(e) => setOptionId(+e.target.value)}
               >
-                <option className='flex items-center justify-center w-full'>
+                <option
+                  className='flex items-center justify-center w-full'
+                  value={0}
+                >
                   -
                 </option>
                 {options.map(
@@ -56,10 +86,10 @@ export const DishCard: FC<IDishProps> = ({
           )}
         </div>
 
-        <div className='flex items-start sm:items-center justify-start sm:justify-between w-full flex-col sm:flex-row'>
+        <div className='flex flex-col items-start justify-start w-full sm:items-center sm:justify-between sm:flex-row'>
           <div className='flex flex-col-reverse items-start justify-start w-full sm:justify-between sm:items-center sm:w-3/5 sm:flex-row space-y-3 sm:space-y-0'>
-            <p>
-              ${basePrice} • {calorie} Cal.
+            <p className='mt-3 sm:mt-0'>
+              ${totalPrice} • {totalCalories} Cal.
             </p>
 
             <select
@@ -74,7 +104,7 @@ export const DishCard: FC<IDishProps> = ({
           </div>
 
           <OrderButton
-            inner='order'
+            inner='add'
             onClick={() => console.log('order button clicked')}
           />
         </div>
--- src/components/OrderButton.tsx
+++ src/components/OrderButton.tsx
@@ -12,7 +12,7 @@ export const OrderButton: FC<IOrderButtonProps> = ({ inner, onClick }) => {
     <motion.button
       whileTap={{ scale: 0.96 }}
       onClick={onClick}
-      className='bg-gray-900 cursor-pointer px-2 py-1 capitalize hover:bg-gray-800 text-center text-white rounded-full shadow flex items-center justify-center text-xs tracking-wide font-bold space-x-1 cst-transition sm:mt-0 mt-2'
+      className='bg-gray-800 cursor-pointer px-2 py-1 capitalize hover:bg-gray-700 text-center text-white rounded-full shadow flex items-center justify-center text-xs tracking-wide font-bold space-x-2 cst-transition sm:mt-0 mt-2 focus:outline-none'
     >
       <div className='flex items-center justify-center w-4 h-4 text-black bg-white rounded-full'>
         <AiOutlinePlus className='w-3 h-3' />
